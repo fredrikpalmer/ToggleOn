aa3c1addd03548d8d77403ee651496398e556e2a
fredrikpalmer <fredrik.palmer@telia.com>
2024-02-17 16:43:56 +0100

    Fix sql query

--- src/ToggleOn.EntityFrameworkCore.SqlServer/Migrations/ExternalIdentity.sql
+++ src/ToggleOn.EntityFrameworkCore.SqlServer/Migrations/ExternalIdentity.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS(SELECT * FROM [sys].[database_principals] WHERE [type] = N'S' AND [name] = 'app-toggleon-api-dev')
+IF NOT EXISTS(SELECT * FROM [sys].[database_principals] WHERE [name] = 'app-toggleon-api-dev')
 BEGIN
 	CREATE USER [app-toggleon-api-dev] FROM  EXTERNAL PROVIDER  WITH DEFAULT_SCHEMA=[dbo]
 	ALTER ROLE db_datareader ADD MEMBER [app-toggleon-api-dev];
